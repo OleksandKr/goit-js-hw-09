f0368a7fc65f66c8d8bb96de679b99fbd4b5a090
OleksandKr <cfif357@ukr.net>
2023-05-04 11:13:07 +0300

    executed-task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const COLOR_CHANGE_TIME = 1000;
+let idTimer = null;
 
+const refs = {
+  bodyRef: document.querySelector('body'),
+  buttonStartRef: document.querySelector('[data-start]'),
+  buttonStopRef: document.querySelector('[data-stop]'),
+};
+
+refs.buttonStopRef.disabled = true;
+refs.buttonStartRef.addEventListener('click', onClickStart);
+refs.buttonStopRef.addEventListener('click', onClickStop);
+
+function onClickStart() {
+  idTimer = setInterval(getBgrColor, COLOR_CHANGE_TIME);
+  refs.buttonStartRef.toggleAttribute('disabled');
+  refs.buttonStopRef.removeAttribute('disabled');
+}
+
+function onClickStop() {
+  clearInterval(idTimer);
+  refs.buttonStopRef.toggleAttribute('disabled');
+  refs.buttonStartRef.toggleAttribute('disabled');
+}
+
+function getBgrColor() {
+  refs.bodyRef.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
