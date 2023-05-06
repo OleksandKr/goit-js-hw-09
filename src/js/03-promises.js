0a156e5aa8bac36b5b6ca555dc5ab44f3d3f29cc
OleksandKr <cfif357@ukr.net>
2023-05-06 18:45:14 +0300

    UpDate-task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formRef = document.querySelector('.form');
+formRef.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(eve) {
+  eve.preventDefault()
+  let delay = Number(formRef.delay.value);
+
+  for (let id = 1; id <= formRef.amount.value; id += 1) {
+    createPromise(id, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += Number(formRef.step.value);
+  };
+};
+
 function createPromise(position, delay) {
+  const object = { position, delay };
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(object);
+      } else {
+        reject(object);
+      }
+    }, delay);
+  });
+};
